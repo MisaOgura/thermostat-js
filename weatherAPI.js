7e5a30c44c70de1354f7db431d0220df54d871ca
Misa Ogura <misa.ogura01@gmail.com>
2016-03-31 10:10:56 +0100

    Experimenting with weatherAPIT

--- weatherAPI.js
+++ weatherAPI.js
@@ -5,14 +5,17 @@ var request = {
   url: "http://api.openweathermap.org/data/2.5/weather",
   data: {
     id: "2643743",
-    APPID: "YOURAPI"
+    APPID: "99de480d57598b514395bc959524e5b9"
   },
   type: "GET",
   dataType: "json"
 };
 
-$.ajax(request).done(function(remoteData){
-  notParsedJson = remoteData;
+$.ajax({
+  type: 'GET',
+  url: 'http://api.openweathermap.org/data/2.5/weather?id=2643743&APPID=99de480d57598b514395bc959524e5b9',
+  success: function(data){
+    console.log('success', data);
+    console.log(data.name + ':' + data.weather);
+  }
 });
-
-$("#weather_result").text(notParsedJson);
